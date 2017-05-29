c8c51a8a6e796beb7b5fdc37f217ce2ca18d98e3
nach7vos <joshwow91@gmail.com>
2017-05-29 11:36:16 +0200

    endgame test

---
+++ src/view/end_game_view.java
@@ -0,0 +1,36 @@
+package view;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class end_game_view {
+	public void end_game_show(){
+		Stage endStage = new Stage();
+		GridPane grid = new GridPane();
+		Button btn_test = new Button("Click me");
+		
+		grid.setId("grid_win");
+		
+		btn_test.setOnAction(e -> { 
+			try{
+				grid.setId("grid_lose");
+			}catch(Exception b){
+				b.printStackTrace();
+		}});
+		
+		grid.add(btn_test, 0, 0);
+		
+		Scene end_scene = new Scene(grid, 415, 300);
+		end_scene.getStylesheets().addAll(this.getClass().getResource("end_game_style.css").toExternalForm());
+		
+		endStage.setTitle("Hervat een spel");
+		endStage.setScene(end_scene);
+		endStage.show();
+	}
+}

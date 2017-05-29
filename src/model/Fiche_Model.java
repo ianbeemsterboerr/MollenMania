f1ff0b10c6eeabb4d4a0c1a92bd3ef7c97e31dce
Wessel Bruijnes <Wesselbr@gmail.com>
2017-05-29 11:45:44 +0200

    FicheModel verengelst

--- src/model/Fiche_Model.java
+++ src/model/Fiche_Model.java
@@ -13,28 +13,28 @@ public class Fiche_Model {
 
     private Random random = new Random();
     private ArrayList<Integer> openFiche = new ArrayList<Integer>();
-    private ArrayList<Integer> geslotenFiche = new ArrayList<Integer>();
+    private ArrayList<Integer> closedFiche = new ArrayList<Integer>();
 
     public void initFiches(){
-        geslotenFiche.add(1);
-        geslotenFiche.add(2);
-        geslotenFiche.add(2);
-        geslotenFiche.add(3);
-        geslotenFiche.add(3);
-        geslotenFiche.add(4);
+        closedFiche.add(1);
+        closedFiche.add(2);
+        closedFiche.add(2);
+        closedFiche.add(3);
+        closedFiche.add(3);
+        closedFiche.add(4);
     }
 
-    public int kiesFiche() {
-        int getal = random.nextInt(geslotenFiche.size());
-        int fiche = geslotenFiche.get(getal);
-        openFiche.add(geslotenFiche.get(getal));
-        geslotenFiche.remove(getal);
+    public int chooseFiche() {
+        int getal = random.nextInt(closedFiche.size());
+        int fiche = closedFiche.get(getal);
+        openFiche.add(closedFiche.get(getal));
+        closedFiche.remove(getal);
         return fiche;
 
     }
 
-    public void fichesCheck(){
-        if(geslotenFiche.size()==0){
+    public void ficheCheck(){
+        if(closedFiche.size()==0){
             System.out.println("Fiches resetten");
             resetFiches();
         }
@@ -43,7 +43,7 @@ public class Fiche_Model {
 
     public void resetFiches(){
         for(int ficheNr : openFiche){
-            geslotenFiche.add(ficheNr);
+            closedFiche.add(ficheNr);
         }
         openFiche.clear();
     }
---
+++ src/model/Pion.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Created by Wessel on 29-5-2017.
+ */
+public class Pion {
+    private int playerID
+
+
+
+
+}

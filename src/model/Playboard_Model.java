0da7216d0f02606c09f4e8c5cbaa7342c65777de
Wessel Bruijnes <Wesselbr@gmail.com>
2017-06-09 14:09:20 +0200

    bezig met playboard

--- src/model/Playboard_Model.java
+++ src/model/Playboard_Model.java
@@ -3,7 +3,6 @@ package model;
 
 import model.Velden.Molshoop_Veld;
 import model.Velden.Normaalveld_Veld;
-import model.Velden.SpeciaalVeld_Veld;
 
 import java.util.ArrayList;
 
@@ -13,8 +12,29 @@ import java.util.ArrayList;
 
 public class Playboard_Model  {
 
-    Niveau_Model n1 = new Niveau_Model(new ArrayList<>(), new ArrayList<>(),new ArrayList<>());
+    Niveau_Model niveau1 = new Niveau_Model(new ArrayList<>(), new ArrayList<>(),new ArrayList<>());
+    Niveau_Model niveau2 = new Niveau_Model(new ArrayList<>(), new ArrayList<>(),new ArrayList<>());
+    Niveau_Model niveau3 = new Niveau_Model(new ArrayList<>(), new ArrayList<>(),new ArrayList<>());
 
-    n1.getMolshoop();
+
+    public Playboard_Model(){
+
+            niveau1.getMolshoop().add(new Molshoop_Veld(0,0,0));
+            
+
+            for (int i = -4; i < 4; i++ ){
+                for(int j = -4; i < 4; i++){
+                    for( int n = -4; n < 4; i++){
+                        niveau1.getNormaal().add(new Normaalveld_Veld(i,j,n));
+                        niveau2.getNormaal().add(new Normaalveld_Veld(i,j,n));
+                        niveau3.getNormaal().add(new Normaalveld_Veld(i,j,n));
+                    }
+                }
+
+        }
+
+
+
+    }
 
 }

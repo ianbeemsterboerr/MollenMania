07dc1db714da2d4cc76d4dcfb583cacc8af5c55d
Wessel Bruijnes <Wesselbr@gmail.com>
2017-06-16 12:29:18 +0200

    Delen van fiche controller en spelbord controller gemaakt

--- src/controller/Mol_Controller.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-/**
- * Created by Wessel on 16-6-2017.
- */
-public class Mol_Controller {
-}
--- src/controller/Spelbord_Controller.java
+++ src/controller/Spelbord_Controller.java
@@ -1,7 +1,8 @@
 package controller;
 
+import model.Mol_Model;
+import model.Playboard_Model;
 import model.Spel_Model;
-import model.Spelbord_Model;
 
 /**
  * Created by Wessel on 15-6-2017.
@@ -10,5 +11,29 @@ public class Spelbord_Controller {
 
 
 
+public void verplaatsMol (Spel_Model spelModel, Mol_Model molModel, int [] eindpunt){
+
+
+
+
+}
+
+public boolean zetGeldig(Mol_Model mol, int [] eindPunt, int ficheNR){
+    //bepaal delta Coordinaten:
+    int deltaCoord[] = new int[3];
+    for (int i = 0; i < eindPunt.length;i++ ){
+        deltaCoord[i] = eindPunt[i] - mol.getCoord()[i];
+    }
+    //Bepaal of Rechte lijn en stappen gelijk aan ficheNR
+    if (deltaCoord[0] == 0 || deltaCoord[1] == 0 || deltaCoord[2] == 0){
+       if (deltaCoord[0] == ficheNR || deltaCoord[1] == ficheNR || deltaCoord[2] == ficheNR){
+           if (deltaCoord[0] == (-1*ficheNR) || deltaCoord[1] == (-1*-ficheNR) || deltaCoord[2] == (-1*ficheNR){
+               return true;
+           }
+    }
+    else {
+           return false;
+       }
+}
 
 }

38cbdea70bb9559a23e57388ace06f9c1f51d981
itje023 <ian.beemsterboer@outlook.com>
2017-06-16 14:01:12 +0200

    Spelsavecontroller gemaakt

--- src/controller/hervatten_game_control.java
+++ src/controller/hervatten_game_control.java
@@ -1,7 +1,10 @@
 package controller;
 
+import javafx.stage.FileChooser;
 import model.Spel_Model;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,6 +15,9 @@ public class hervatten_game_control {
 
 	}
 
+	public String kiesFile() {
+		return null;
+	}
 	public Spel_Model spelLaden() {
 		Spel_Model spelModel;
 		try {

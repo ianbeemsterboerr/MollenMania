0233b23d40808493b9224111f8462b380da975e1
Wessel Bruijnes <Wesselbr@gmail.com>
2017-06-16 12:43:45 +0200

    paar errors

--- src/controller/SpelbordController.java
+++ src/controller/SpelbordController.java
@@ -22,16 +22,15 @@ public boolean zetGeldig(Mol_Model mol, int [] eindPunt, int ficheNR){
         deltaCoord[i] = eindPunt[i] - mol.getCoord()[i];
     }
     //Bepaal of Rechte lijn en stappen gelijk aan ficheNR
-    if (deltaCoord[0] == 0 || deltaCoord[1] == 0 || deltaCoord[2] == 0){
-       if (deltaCoord[0] == ficheNR || deltaCoord[1] == ficheNR || deltaCoord[2] == ficheNR){
-           if (deltaCoord[0] == (-1*ficheNR) || deltaCoord[1] == (-1*-ficheNR) || deltaCoord[2] == (-1*ficheNR){
-               return true;
-           }
-    }
-    else {
-           return false;
-       }
-}
+    if (deltaCoord[0] == 0 || deltaCoord[1] == 0 || deltaCoord[2] == 0) {
+        if (deltaCoord[0] == ficheNR || deltaCoord[1] == ficheNR || deltaCoord[2] == ficheNR) {
+            if (deltaCoord[0] == (-1 * ficheNR) || deltaCoord[1] == (-1 * -ficheNR) || deltaCoord[2] == (-1 * ficheNR)) {
+                return true;
+            }
+        }
 
+    }
+        return false;
+    
 }
 }

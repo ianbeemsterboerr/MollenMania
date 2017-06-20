1865480a3c8ab5060b4330b34ddee5c51a078b2d
RdenBlaauwen <rdenblaauwen@gmail.com>
2017-06-20 12:02:54 +0200

    handleiding toegevoegd

--- src/view/HandleidingView.java
+++ src/view/HandleidingView.java
@@ -11,9 +11,17 @@ import javafx.scene.image.*;
  * Created by Robert on 6/20/2017.
  */
 public class HandleidingView {
-    private Image pag1 = new Image("");
+    private Image[] paginas = new Image[4];
+    private int huidigePagina=0;
+    ImageView imgPane;
+
 
     public HandleidingView(){
+        paginas[0] = new Image("../img/handleiding_pag1.png");
+        paginas[1] = new Image("../img/handleiding_pag2.png");
+        paginas[2] = new Image("../img/handleiding_pag3.png");
+        paginas[3] = new Image("../img/handleiding_pag4.png");
+
         double button_width = 150.0;
         Stage handleidingStage = new Stage();
         HBox hbox_handleiding = new HBox();
@@ -26,12 +34,35 @@ public class HandleidingView {
         hbox_handleiding.getChildren().addAll(btn_vorige,btn_volgende);
         hbox_handleiding.setAlignment(Pos.CENTER);
 
+        imgPane = new ImageView(paginas[huidigePagina]);
+        imgPane.setFitWidth(1440);
+        imgPane.setPreserveRatio(true);
+
         BorderPane borderPane = new BorderPane();
         borderPane.setBottom(hbox_handleiding);
+        borderPane.setCenter(imgPane);
 
         Scene handleiding_scene = new Scene(borderPane, 250, 85);
         handleidingStage.setScene(handleiding_scene);
         handleidingStage.setTitle("Mollenmania - handleiding");
         handleidingStage.show();
+    }
+
+    private void vorige(){
+        if(huidigePagina==0){
+            huidigePagina=3;
+        } else {
+            huidigePagina--;
+        }
+        imgPane.setImage(paginas[huidigePagina]);
+    }
+
+    private void volgende(){
+        if(huidigePagina==3){
+            huidigePagina=0;
+        } else{
+            huidigePagina++;
+        }
+        imgPane.setImage(paginas[huidigePagina]);
     }
 }

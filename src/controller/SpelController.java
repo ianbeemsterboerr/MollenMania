d73f4513386781eabac477067baae3fb09c6d338
Wessel Bruijnes <Wesselbr@gmail.com>
2017-06-20 14:30:50 +0200

    spelcontroller toegevoegd

---
+++ src/controller/SpelController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import model.Spel_Model;
+
+/**
+ * Created by Wessel on 20-6-2017.
+ */
+public class SpelController {
+
+
+    public void Spel (Spel_Model spelModel){
+
+        //Hierin wordt de beurt logica herhaald
+        do {
+
+
+        }
+        //zodra iemand op de goudenschep komt wordt dit true en stopt de loop
+        while (!spelModel.getSpelbord().getNiveau4().getGoudenSchep().get(0).isEindeSpel());
+        //Laat einde scherm zien
+    }
+
+
+}

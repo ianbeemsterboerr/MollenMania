f21d8ccda1636ceeccc0d7f038ca73c50c452969
RdenBlaauwen <rdenblaauwen@gmail.com>
2017-06-20 21:39:47 +0200

    lobbyController en lobbyView toegevoegd, bevate haast niks tho

---
+++ src/controller/LobbyController.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by Robert on 6/20/2017.
+ */
+public class LobbyController {
+}
---
+++ src/view/LobbyView.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by Robert on 6/20/2017.
+ */
+public class LobbyView {
+}

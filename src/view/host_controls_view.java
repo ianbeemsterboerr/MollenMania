59261e2858fca9c956d845fc1394558e142a1f99
nach7vos <joshwow91@gmail.com>
2017-06-20 13:49:16 +0200

    lol

--- src/view/host_controls_view.java
+++ src/view/host_controls_view.java
@@ -1,6 +1,5 @@
 package view;
 
-import controller.molserver;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -36,7 +35,6 @@ public class host_controls_view {
 
 		btn_host.setOnAction(e -> {
 			try{
-				new molserver();
 				connectStage.close();
 				new connect_host_view(this.aantalSpelers());
 				//System.out.println(this.aantalSpelers());

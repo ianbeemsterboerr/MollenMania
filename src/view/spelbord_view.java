ed5ae0751fef3377cb7b5adf1535e1890308026d
bastiaan.veldhuisen <bastiaan.veldhuisen@hotmail.nl>
2017-06-20 15:46:16 +0200

    spelbord view geupdate

--- src/view/spelbord_view.java
+++ src/view/spelbord_view.java
@@ -1,25 +1,58 @@
 package view;
 
-import javafx.geometry.HPos;
-import javafx.geometry.Rectangle2D;
-import javafx.geometry.VPos;
+import controller.Bordspel_Controller;
+import controller.Bordspel_Interface;
+import controller.Player_Observer;
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.Speler_Model;
 import model.Velden.VeldKnop;
 
-public class spelbord_view {
+import java.rmi.RemoteException;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+public class spelbord_view extends UnicastRemoteObject implements Player_Observer {
 	int[] intArray1;
 	int[] intArray2;
-	
-	
+	GridPane player_1;
+	GridPane player_2;
+	GridPane player_3;
+	GridPane player_4;
+	private static final long serialVersionUID = 1L;
+	private ArrayList<Speler_Model> players;
+
+	protected spelbord_view(int port) throws RemoteException {
+		super(port);
+	}
+
+	protected spelbord_view(int port, RMIClientSocketFactory csf, RMIServerSocketFactory ssf) throws RemoteException {
+		super(port, csf, ssf);
+	}
+
+	@Override
+	public void modelChanged(Bordspel_Interface playable) throws RemoteException {
+
+	}
+
+	@Override
+	public boolean isEnabled() throws RemoteException {
+		return false;
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) throws RemoteException {
+
+	}
+
 	public int[] getIntArray1() {
 		return intArray1;
 	}
@@ -40,15 +73,77 @@ public class spelbord_view {
 	}
 
 
-	public Stage spelbord_show() {
+	public Stage spelbord_show(Bordspel_Controller bs_controller, Bordspel_Interface bs_interface) throws RemoteException{
+
+		try {
+			bs_interface.addObserver(this);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+		players = bs_interface.playerList();
 		Stage primaryStage = new Stage();
 		GridPane root = new GridPane();
 
-		VBox spelerData1 = new VBox(8);
-		VBox spelerData2 = new VBox(8);
-		VBox spelerData3 = new VBox(8);
-		VBox spelerData4 = new VBox(8);
-		
+		VBox left = new VBox();
+		left.setPadding(new Insets(20, 20, 20, 20));
+		left.setSpacing(300.0);
+		VBox right = new VBox();
+		right.setPadding(new Insets(20, 20, 20, 20));
+		right.setSpacing(300.0);
+
+		if(players.size() == 2){
+			this.player_1 = this.createUserPanel(players.get(0));
+			this.player_2 = this.createUserPanel(players.get(1));
+			left.getChildren().addAll(player_1);
+			right.getChildren().addAll(player_2);
+
+			this.player_1.setAlignment(Pos.TOP_LEFT);
+			this.player_2.setAlignment(Pos.TOP_RIGHT);
+		} else if(players.size() == 3){
+			this.player_1 = this.createUserPanel(players.get(0));
+			this.player_2 = this.createUserPanel(players.get(1));
+			this.player_3 = this.createUserPanel(players.get(2));
+
+			left.getChildren().addAll(player_1, player_3);
+			right.getChildren().addAll(player_2);
+
+			this.player_1.setAlignment(Pos.TOP_LEFT);
+			this.player_2.setAlignment(Pos.TOP_RIGHT);
+			this.player_3.setAlignment(Pos.BOTTOM_LEFT);
+		} else if(players.size() == 4){
+			this.player_1 = this.createUserPanel(players.get(0));
+			this.player_2 = this.createUserPanel(players.get(1));
+			this.player_3 = this.createUserPanel(players.get(2));
+			this.player_4 = this.createUserPanel(players.get(3));
+
+			left.getChildren().addAll(player_1, player_3);
+			right.getChildren().addAll(player_2, player_4);
+
+			this.player_1.setAlignment(Pos.TOP_LEFT);
+			this.player_2.setAlignment(Pos.TOP_RIGHT);
+			this.player_3.setAlignment(Pos.BOTTOM_LEFT);
+			this.player_4.setAlignment(Pos.BOTTOM_RIGHT);
+		} else {
+			System.out.println("nope");
+		}
+
+//		GridPane player_1 = this.createUserPanel(players.get(0));
+//		GridPane player_2 = this.createUserPanel(players.get(1));
+//		GridPane player_3 = this.createUserPanel(players.get(2));
+//		GridPane player_4 = this.createUserPanel(players.get(3));
+//
+//		this.player_1.setAlignment(Pos.TOP_LEFT);
+//		this.player_2.setAlignment(Pos.TOP_RIGHT);
+//		this.player_3.setAlignment(Pos.BOTTOM_LEFT);
+//		this.player_4.setAlignment(Pos.BOTTOM_RIGHT);
+
+
+		BorderPane moap = new BorderPane();
+		moap.setLeft(left);
+		moap.setRight(right);
+
+
 		int numRows = 12;
 		int numCols = 41;
 		int ficheRowStart = 12;
@@ -69,12 +164,7 @@ public class spelbord_view {
 		Label naamSpeler2 = new Label(); 
 		Label naamSpeler3 = new Label(); 
 		Label naamSpeler4 = new Label(); 
-		
-		spelerData1.getChildren().addAll(speler1, fiches1, molSpeler1, naamSpeler1);
-		spelerData2.getChildren().addAll(speler2, fiches2, molSpeler2, naamSpeler2);
-		spelerData3.getChildren().addAll(speler3, fiches3, molSpeler3, naamSpeler3);
-		spelerData4.getChildren().addAll(speler4, fiches4, molSpeler4, naamSpeler4);
-	
+
 		Button btn_close = new Button("exit");
 		Button btn_minimize = new Button("-");
 		VeldKnop[] buttonArray = new VeldKnop[61];
@@ -150,10 +240,6 @@ public class spelbord_view {
 		
 		root.add(btn_close, 41, 0);
 		root.add(btn_minimize, 40, 0);
-		root.add(spelerData1, 10, 2);
-		root.add(spelerData2, 10, 9);
-		root.add(spelerData3, 35, 2);
-		root.add(spelerData4, 35, 9);
 		
 		buttonArray[0 ].setOnAction(e -> {System.out.println(buttonArray[0 ].getX()+" "+buttonArray[0 ].getY()+" "+buttonArray[0 ].getZ());});
 		buttonArray[1 ].setOnAction(e -> {System.out.println(buttonArray[1 ].getX()+" "+buttonArray[1 ].getY()+" "+buttonArray[1 ].getZ());});
@@ -252,6 +338,37 @@ public class spelbord_view {
 		primaryStage.setScene(scene);
 
 		return primaryStage;
+	}
+
+	public GridPane createUserPanel(Speler_Model sm){
+		/*
+		 * maybe each panel should be owned by a user?! watcha think dog.
+		 */
+
+		String speler_naam = sm.getUsername();
+		String mol_count = Integer.toString(sm.getMol_list().size());
+		// int fiche_count = sm.getFicheList().size();
+
+		GridPane grid = new GridPane();
+
+		Label username_lbl = new Label(speler_naam);
+		Label aantal_fiche_lbl = new Label("5");
+		Label aantal_mol_lbl = new Label(mol_count);
+
+		Button fiche_btn = new Button("Fiche");
+		Button mol_btn = new Button("Mol");
+		Button klaar_btn = new Button("Klaar");
+
+		grid.add(username_lbl, 0, 0);
+		grid.add(mol_btn, 0, 1);
+		grid.add(aantal_mol_lbl, 1, 1);
+		grid.add(fiche_btn, 0, 2);
+		grid.add(aantal_fiche_lbl, 1, 2);
+		grid.add(klaar_btn, 0, 3);
+		grid.setHgap(10.0);
+		grid.setVgap(10.0);
+
+		return grid;
 	}
 
 	// TODO Auto-generated method stub

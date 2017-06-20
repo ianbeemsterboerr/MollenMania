fce4bbfa9e128b784e53642f59b3fa98a0df8a83
itje023 <ian.beemsterboer@outlook.com>
2017-06-20 14:09:15 +0200

    HervatGameView returned nu een string van de gekozen file

--- src/view/HervatGameView.java
+++ src/view/HervatGameView.java
@@ -12,14 +12,31 @@ import java.io.File;
  * Created by Ian on 6/16/2017.
  */
 public class HervatGameView {
+    SpelHervattenController ctrl;
     public HervatGameView(SpelHervattenController ctrl) {
-        show();
+        System.out.print(show());
+        this.ctrl = ctrl;
     }
-    public void show() {
+
+    /**
+     * Geeft de filechooser weer waarin alleen .sav files kunnen worden geselecteerd en returned een filepath string.
+     *
+     * @return filePath String van de geselecteerde file.
+     */
+    public String show() {
         Stage stage = new Stage();
         FileChooser fc = new FileChooser();
-        fc.showOpenDialog(stage);
 
+        //zorg dat de filechooser alleen .sav files accepteert.
+        FileChooser.ExtensionFilter extentionFilter = new FileChooser.ExtensionFilter("SAV files .sav", "*.sav");
+        fc.getExtensionFilters().add(extentionFilter);
+
+        //initial directory = de home folder van de user.
+        String currentDir = System.getProperty("user.home");
+        File directoryPath = new File(currentDir);
+        fc.setInitialDirectory(directoryPath);
+        String filePath = fc.showOpenDialog(stage).getAbsolutePath();
+        return filePath;
     }
 
 

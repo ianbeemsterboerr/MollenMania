26c47f2864573b14aeff044b085a9521f70bd9d5
Wessel Bruijnes <Wesselbr@gmail.com>
2017-06-21 20:16:14 +0200

    molcontroller/spelcontroller

--- src/controller/SpelController.java
+++ src/controller/SpelController.java
@@ -2,15 +2,21 @@ package controller;
 
 import model.Spel_Model;
 import model.Speler_Model;
+import model.Velden.VeldKnop;
 
 /**
  * Created by Wessel on 20-6-2017.
  */
 public class SpelController {
 
-
+    MolController molController = new MolController();
     public void Spel (Spel_Model spelModel){
+        //spelervolgorde wordt bepaalt
+        int spelerAanBeurt = 0;
+        //Mollen worden neergezet
+         while (spelModel.getSpeler().get(spelerAanBeurt).getMol_list().size() < molController.aantalMollen(spelModel)){
 
+         }
         //Hierin wordt de beurt logica herhaald
         do {
 

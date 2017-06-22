0314c33e95febe813116142b94b2b1436f461180
RdenBlaauwen <rdenblaauwen@gmail.com>
2017-06-22 12:12:44 +0200

    Lobby_View en mol_client aangepast zodat je niet meer op de lijst hoeft te klikken in de lobby

--- src/controller/Mol_Client.java
+++ src/controller/Mol_Client.java
@@ -1,8 +1,10 @@
 package controller;
 
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import model.Spelbord_Model;
 import model.Speler_Model;
 import view.InstellingenView;
 import view.Lobby_View;
@@ -10,28 +12,34 @@ import view.Lobby_View;
 public class Mol_Client {
 
 	private InstellingenView instellingenView;
-	
+	private Speler_Model speler_model;
+	private Bordspel_Interface userStub;
+
 	public Mol_Client(String username){
 		//this.instellingenView=instellingenView;
 		try {
-			// get access to the RMI registry on the remote server	
+			// get access to the RMI registry on the remote server
 			// if server on another machine: provide that machine's IP address. Default port  1099
 			Registry registry = LocateRegistry.getRegistry("127.0.0.1");
-			
-            //CALL OBJECT FROM INTERFACE IMPLEMENTATIONNN
+
+			//CALL OBJECT FROM INTERFACE IMPLEMENTATIONNN
 			// get remote interface object from registry
-			Bordspel_Interface userStub = (Bordspel_Interface) registry.lookup("Spelbord_Model");
+			this.userStub = (Bordspel_Interface) registry.lookup("Spelbord_Model");
 			Bordspel_Controller bs_controller = new Bordspel_Controller(userStub);
-			
+
 			int spid = userStub.playerList().size()+1;
 			//System.out.println(userStub.getClientHost());
-			Speler_Model sm = new Speler_Model(username, spid);
-			
-			bs_controller.checkPlayerList(4, sm);
-			
-			new Lobby_View(userStub, bs_controller);
+			this.speler_model = new Speler_Model(username, spid);
+
+			bs_controller.checkPlayerList(4, speler_model);
+
+			new Lobby_View(userStub, bs_controller, this);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+	}
+
+	public void notifyKlaar() throws RemoteException {
+		this.userStub.addSpelerReady(this.speler_model);
 	}
 }
--- src/view/Lobby_View.java
+++ src/view/Lobby_View.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 import controller.Bordspel_Interface;
+import controller.Mol_Client;
 import controller.Player_Observer;
 import controller.Bordspel_Controller;
 import javafx.collections.FXCollections;
@@ -20,43 +21,44 @@ import javafx.stage.Stage;
 import model.Speler_Model;
 
 public class Lobby_View extends UnicastRemoteObject implements Player_Observer {
-	
+
 	//what do i need??!?!?!?
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	Bordspel_Controller bs_controller;
 	Bordspel_Interface bs_interface;
+	private Mol_Client mol_client;
 	ObservableList<Speler_Model> data;
-	
+
 	public Lobby_View() throws RemoteException{
-		
+
 	}
-	
-	public Lobby_View(Bordspel_Interface bs_interface, Bordspel_Controller bs_controller) throws RemoteException{
-		
+
+	public Lobby_View(Bordspel_Interface bs_interface, Bordspel_Controller bs_controller, Mol_Client mol_client) throws RemoteException{
+		this.mol_client=mol_client;
 		//Add this view to observer list
-		
+
 		this.bs_interface = bs_interface;
 		this.bs_controller = bs_controller;
-		
+
 		//view bullshit
-		double button_width = 100.0;									
+		double button_width = 100.0;
 		Stage lobbyStage = new Stage();
 		VBox vbox_hervat_options = new VBox();
 		GridPane grid = new GridPane();
 		TableView<Speler_Model> game_table = new TableView<Speler_Model>();
-		
+
 		Button btn_pion = new Button("KIES PION");
 		Button btn_kleur = new Button("KIES KLEUR");
 		Button btn_klaar = new Button("KLAAR");
 		Button btn_refresh = new Button("REFRESH");
-		
+
 		ObservableList<Speler_Model> data = FXCollections.observableArrayList(bs_interface.playerList());
 		game_table.setEditable(false);
-		
+
 		TableColumn<Speler_Model, Integer> player_id_col = new TableColumn<Speler_Model, Integer>("PLAYER ID");
 		TableColumn<Speler_Model, String> player_name_col = new TableColumn<Speler_Model, String>("PLAYER NAME");
 		game_table.setItems(data);
@@ -64,22 +66,22 @@ public class Lobby_View extends UnicastRemoteObject implements Player_Observer {
 		player_name_col.setMinWidth(25.0);
 		game_table.setMaxWidth(175.0);
 		game_table.setMaxHeight(135.0);
-		
+
 		player_id_col.setCellValueFactory(
-                new PropertyValueFactory<Speler_Model, Integer>("player_id"));
+				new PropertyValueFactory<Speler_Model, Integer>("player_id"));
 		player_name_col.setCellValueFactory(
-                new PropertyValueFactory<Speler_Model, String>("username"));
-		
+				new PropertyValueFactory<Speler_Model, String>("username"));
+
 		game_table.getColumns().addAll(player_id_col, player_name_col);
-		
+
 		btn_pion.setMaxWidth(button_width);
 		btn_kleur.setMaxWidth(button_width);
 		btn_refresh.setMaxWidth(button_width);
 		btn_klaar.setMaxWidth(button_width);
-		
+
 		vbox_hervat_options.setSpacing(5.0);
 		vbox_hervat_options.getChildren().addAll(btn_pion, btn_kleur, btn_klaar, btn_refresh);
-		
+
 		btn_kleur.setOnAction(e->{
 			spelbord_view test;
 			try {
@@ -90,32 +92,33 @@ public class Lobby_View extends UnicastRemoteObject implements Player_Observer {
 				e1.printStackTrace();
 			}
 		});
-				
-		btn_refresh.setOnAction(e -> { 
+
+		btn_refresh.setOnAction(e -> {
 			try{
 				ObservableList<Speler_Model> data_new = FXCollections.observableArrayList(bs_interface.playerList());
 				game_table.setItems(data_new);
 			}catch(Exception b){
 				b.printStackTrace();
-		}});
-		
-		btn_klaar.setOnAction(e -> { 
+			}});
+
+		btn_klaar.setOnAction(e -> {
 			try{
 				//new SpelbordView(this.bs_controller, this.bs_interface);
-				this.bs_interface.addSpelerReady(game_table.getSelectionModel().getSelectedItem().getMyself());
-				System.out.println("Player: " + game_table.getSelectionModel().getSelectedItem().getPlayer_id() + " is ready.");
+				//this.bs_interface.addSpelerReady(game_table.getSelectionModel().getSelectedItem().getMyself());
+				//System.out.println("Player: " + game_table.getSelectionModel().getSelectedItem().getPlayer_id() + " is ready.");
+				this.mol_client.notifyKlaar();
 				System.out.println(this.bs_interface.readyList().size());
 			}catch(Exception b){
 				b.printStackTrace();
-		}});
-		
+			}});
+
 		grid.setHgap(10);
-	    grid.setPadding(new Insets(5, 5, 5, 5));
-	    grid.add(game_table, 0, 0);
-	    grid.add(vbox_hervat_options, 1, 0);
-		
+		grid.setPadding(new Insets(5, 5, 5, 5));
+		grid.add(game_table, 0, 0);
+		grid.add(vbox_hervat_options, 1, 0);
+
 		Scene lobby_scene = new Scene(grid, 380, 250);
-		
+
 		lobbyStage.setTitle("Lobby");
 		lobbyStage.setScene(lobby_scene);
 		lobbyStage.show();
@@ -124,7 +127,7 @@ public class Lobby_View extends UnicastRemoteObject implements Player_Observer {
 	@Override
 	public void modelChanged(Bordspel_Interface playable) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -136,6 +139,6 @@ public class Lobby_View extends UnicastRemoteObject implements Player_Observer {
 	@Override
 	public void setEnabled(boolean enabled) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 }

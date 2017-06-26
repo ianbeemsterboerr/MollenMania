bc8b3c63efa5e8c887570121632d8fe803cf70c9
nach7vos <joshwow91@gmail.com>
2017-06-26 10:26:01 +0200

    map changes

--- src/model/Spelbord_Model.java
+++ src/model/Spelbord_Model.java
@@ -15,60 +15,10 @@ public class Spelbord_Model implements Bordspel_Interface{
 	private Playboard_Model pmo = new Playboard_Model();
 	private int aanDeBeurt;
 	private int bordMax;
-	private int maxMollen;
-	private int beurtIndex=0;
-
-	//private Niveau_Model niveau1 = new Niveau_Model(); niveau's meoten gemaakt worden.
-	private BeurtStatus beurtStatus;
-
-	public Spelbord_Model(int maxSpelers){
-		this.beurtStatus = BeurtStatus.LOBBY;
-		this.bordMax=maxSpelers;
-		switch (maxSpelers){
-			case 2:
-				this.maxMollen =10;
-				break;
-			case 3:
-				this.maxMollen =8;
-				break;
-			case 4:
-				this.maxMollen =6;
-				break;
-			case 1:
-				System.out.println(this.getClass().toString()+": max spelers te laag. Setup failed.");
-				break;
-			default:
-				System.out.println(this.getClass().toString()+": max spelers te hoog: "+maxSpelers+", mag niet meer zijn dan 4. Setup failed.");
-		}
-	}
-
-<<<<<<< HEAD
-		
-=======
->>>>>>> 27d0c4a75751f768a175231b54dfb3284a0d002c
-	public Spelbord_Model(String saveNaam){
-		System.out.println(this.getClass().toString()+": savenaam is "+saveNaam);
-		this.beurtStatus = BeurtStatus.LOBBY;
-	}
-
-	public int getMaxMollen() throws RemoteException{
-		return this.maxMollen;
-	}
-
-	public BeurtStatus getBeurtStatus() throws RemoteException {
-		return beurtStatus;
-	}
-
-	public void setBeurtStatus(BeurtStatus beurtStatus) throws RemoteException {
-		this.beurtStatus = beurtStatus;
-	}
-<<<<<<< HEAD
 
 	public Spelbord_Model(){
-		this.beurtStatus = BeurtStatus.LOBBY;
+		
 	}
-=======
->>>>>>> 27d0c4a75751f768a175231b54dfb3284a0d002c
 	
 	public ArrayList<Speler_Model> getPlayers() {
 		return players;
@@ -145,6 +95,10 @@ public class Spelbord_Model implements Bordspel_Interface{
 		this.aanDeBeurt += 1;
 	}
 	
+	public void setBordMax(int m){
+		this.bordMax = m;
+	}
+	
 	@Override
 	public int maxSpelers() throws RemoteException {
 		// TODO Auto-generated method stub
@@ -174,8 +128,15 @@ public class Spelbord_Model implements Bordspel_Interface{
 	}
 
 	@Override
-	public int getBeurtIndex() throws RemoteException {
+	public int getMaxMollen() throws RemoteException {
 		// TODO Auto-generated method stub
-		return this.aanDeBeurt;
+		return 0;
 	}
+
+	@Override
+	public int beurtIndex() throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }

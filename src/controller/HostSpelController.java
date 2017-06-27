ada961595754e1d91bf41751123de36d9119b49e
RdenBlaauwen <rdenblaauwen@gmail.com>
2017-06-27 11:54:25 +0200

    minor change

--- src/controller/HostSpelController.java
+++ src/controller/HostSpelController.java
@@ -2,6 +2,8 @@ package controller;
 
 import view.*;
 
+import java.io.IOException;
+
 /**
  * Created by Robert on 6/18/2017.
  */
@@ -12,6 +14,9 @@ public class HostSpelController {
     public HostSpelController(MainMenuController mainMenuController){
         this.hostSpelView = new HostSpelView(this);
         this.mainMenuController = mainMenuController;
+    }
+
+    public void startServer(){
     }
 
     public MainMenuController getMainMenuController(){

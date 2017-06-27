1f859a015892a8c2e06cf81062e19f07caf80c40
RdenBlaauwen <rdenblaauwen@gmail.com>
2017-06-27 13:05:32 +0200

    mogelijke RMI fix

--- src/controller/HostSpelController.java
+++ src/controller/HostSpelController.java
@@ -17,6 +17,18 @@ public class HostSpelController {
     }
 
     public void startServer(){
+        String IP = "145.101.89.116";
+        System.out.println(this.getClass().toString()+": startServer");
+        String home =System.getProperties().getProperty("java.home");
+        System.out.println("serverstart: "+home);
+        try {
+            //java -Djava.rmi.server.hostname=145.101.89.116 C:/Users/Robert/IdeaProjects/mollenmania/Mol_server/bin/controller/run
+            Runtime runTime = Runtime.getRuntime();
+            Process process = runTime.exec("Javavm -D"+IP);
+            Process process2 = runTime.exec("java -classpath C:/Users/Robert/IdeaProjects/mollenmania/Mol_server/bin//controller/run Run");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public MainMenuController getMainMenuController(){
--- src/controller/SpelFlowController.java
+++ src/controller/SpelFlowController.java
@@ -12,7 +12,6 @@ import java.rmi.RemoteException;
  * Created by Wessel on 26-6-2017.
  */
 public class SpelFlowController {
-public class SpelflowController {
    private MolController molController = new MolController();
    private Bordspel_Controller bsController = new Bordspel_Controller();
    private Fiche_Controller ficheController = new Fiche_Controller();

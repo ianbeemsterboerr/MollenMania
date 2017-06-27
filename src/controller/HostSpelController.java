cdc552b41947cefd64934fa4cbb49dccd841730a
itje023 <ian.beemsterboer@outlook.com>
2017-06-27 18:45:47 +0200

    Wat javadoc toegevoegd.

--- src/controller/HostSpelController.java
+++ src/controller/HostSpelController.java
@@ -5,17 +5,25 @@ import view.*;
 import java.io.IOException;
 
 /**
+ * Start de server op.
  * Created by Robert on 6/18/2017.
  */
 public class HostSpelController {
     private HostSpelView hostSpelView;
     private  MainMenuController mainMenuController;
 
+    /**
+     *
+     * @param mainMenuController De mainmenucontroller die alle user input afhandelt.
+     */
     public HostSpelController(MainMenuController mainMenuController){
         this.hostSpelView = new HostSpelView(this);
         this.mainMenuController = mainMenuController;
     }
 
+    /**
+     * Start de server op met de door de user ingevoerde IP. Dit is de IP waar andere clients kunnen connecten.
+     */
     public void startServer(){
         String IP = "145.101.89.116";
         System.out.println(this.getClass().toString()+": startServer");
